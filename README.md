545bfec98da48272dceed9a9dd95351eff5ae9e3
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-05-04 20:34:02 +0700

    upload source code and various thing

---
+++ README.md
@@ -0,0 +1,7 @@
+### Instruction
+
+1. Make sure your game is able to pair with your game (API coming soon)
+
+2. Start server
+
+3. Enjoy
---
+++ bin/gameServe.js
@@ -0,0 +1,51 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+const PORT = process.env.PORT || 5000;
+console.log(`Hosting from ${process.cwd()}`);
+app.use(require("express").static(process.cwd()));
+
+var roomno = 0;
+let roomLimit = 2;
+
+io.of("/game").on("connection", (socket) => {
+    console.log(`${socket.client.id} connected`);
+
+    let roomList = io.nsps["/game"].adapter.rooms;
+
+    if (
+        roomList["room" + roomno] &&
+        roomList["room" + roomno].length > roomLimit
+    )
+        roomno++;
+
+    let myRoom = "room" + roomno;
+    socket.join(myRoom);
+
+    console.log(`${socket.client.id} in ${myRoom}`);
+
+    socket.on("disconnect", () => {
+        console.log(`${socket.client.id} disconnected`);
+        socket.leaveAll();
+    });
+    socket.on("data", (msg) => {
+        io.of("/game").emit("data", msg);
+    });
+    socket.on("ready", (_) => {
+        // TODO: Do something with msg
+        if (roomList[myRoom])
+            io.of("/game").emit("ready", roomList[myRoom].length);
+    });
+});
+
+let listener;
+let reportFn = () => {
+    console.log(`Listening on localhost:${listener.address().port}`);
+};
+try {
+    listener = http.listen(PORT, reportFn);
+} catch (err) {
+    console.log(`Cannot host on ${PORT}. Using random port...`);
+    listener = http.listen(0, reportFn);
+}
